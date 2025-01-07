6e2e9fa27d2b47e19229b3c5b351f2a786fbde60
Ömer Durmaz (Hepsiburada/Bilgi Teknolojileri) <omer.durmaz@hepsiburada.com>
2025-01-07 13:29:08 +0300

    rename module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,7 @@ android {
 
 dependencies {
 
-    implementation(project(":hbfonts"))
-
+    implementation(project(":hbfontlib"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "HBFontTestApp"
 include(":app")
-include(":hbfonts")
+include(":hbfontlib")
