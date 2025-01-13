96a42c21d19c3b2c252bc87341e6b19001ec5cc6
Ömer Durmaz (Hepsiburada/Bilgi Teknolojileri) <omer.durmaz@hepsiburada.com>
2025-01-13 15:37:08 +0300

    revert changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.omerdurmaz2:hbfontlib:1.0.8")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
